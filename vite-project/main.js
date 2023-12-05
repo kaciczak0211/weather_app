4db395ee6200819d5d7edd335834047e7a6d282b
kaciczak0211 <kaciczak0211@gmail.com>
2023-12-05 22:59:02 +0000

    js_add

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,17 +1,26 @@
 const api_key = "12935843512240dca4b220440230512";
 
 
-fetch(`http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=Shannon`)
-.then(res=>{
-    if(!res.ok){
-        throw new Error("wrong value")
-    }
+const btn = document.getElementById("search");
 
-    return res.json()
+btn.addEventListener("click", function(){
+    const city = document.getElementById("city").value
+    console.log(city)
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city}`)
+    .then(res=>{
+        if(!res.ok){
+            throw new Error("wrong value")
+        }
+    
+        return res.json()
+    })
+    .then(data=>{
+        console.log(data)
+    })
+    .catch(err=>{
+        console.warn(err)
+    })
 })
-.then(data=>{
-    console.log(data)
-})
-.catch(err=>{
-    console.warn(err)
-})
+
+
+
