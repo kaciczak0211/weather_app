ebd7f5801e3ed0813f9402ff05a33f9ca2be7df6
kaciczak0211 <kaciczak0211@gmail.com>
2023-12-26 23:52:56 +0000

    html_file_move

---
+++ main.js
@@ -0,0 +1,57 @@
+const api_key = "12935843512240dca4b220440230512";
+
+
+const btn = document.getElementById("search");
+
+const temp = document.getElementById("temp")
+
+const form = document.getElementById("city")
+const cityName = document.getElementById("city_name")
+
+const info = document.getElementById("info");
+
+const conditionIcon = document.getElementById("conditionIcon");
+
+const conditionTextDiv = document.getElementById("conditionText");
+
+const time = document.getElementById("time")
+
+
+
+
+
+
+btn.addEventListener("click", function(){
+    const city = document.getElementById("city").value
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city}`)
+    .then(res=>{
+        if(!res.ok){
+            throw new Error("wrong value")
+        }
+        return res.json()
+    })
+    .then(data=>{
+        info.style.display = "block"
+        time.innerHTML = `${data.current.last_updated}`
+        cityName.innerHTML = `${data.location.name}, ${data.location.country}`;
+        console.log(data)
+        const temperature = data.current.temp_c
+        temp.innerHTML = temperature + "°C"
+        console.log(temperature)
+        const conditionText = data.current.condition.text;
+        const conditionImgSRC = data.current.condition.icon;
+        let img = document.createElement('img');
+        img.src = conditionImgSRC;
+        console.log(img)
+        console.log(conditionText);
+        console.log(conditionImgSRC);
+        document.getElementById("conditionIcon").appendChild(img);        
+        conditionTextDiv.innerHTML = conditionText;
+    })
+    .catch(err=>{
+        console.warn(err)
+        
+    })
+    document.getElementById("conditionIcon").innerHTML = "";
+    form.value = "";
+})
